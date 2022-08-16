43b55a56da20e20ca9374346b6a60a9fbd233384
Dmitri Chuprin <wassermanum@gmail.com>
2022-08-16 16:14:16 +0600

    minor fixes

--- src/test/java/hexlet/code/FormatterTest.java
+++ src/test/java/hexlet/code/FormatterTest.java
@@ -1,9 +1,7 @@
 package hexlet.code;
 
-import com.fasterxml.jackson.core.JsonGenerator;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.core.type.TypeReference;
-import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
