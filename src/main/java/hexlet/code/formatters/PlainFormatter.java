0ccac9ad46b7caa10e7d6cf90773ee53d3724ff0
Dmitri Chuprin <wassermanum@gmail.com>
2022-08-18 01:57:29 +0600

    debug

--- src/main/java/hexlet/code/formatters/PlainFormatter.java
+++ src/main/java/hexlet/code/formatters/PlainFormatter.java
@@ -22,7 +22,7 @@ public class PlainFormatter {
                         (FIRST_LINE_PART + "added with value: "
 //                                + (isString(x.newValue()) ? "'%s'\n" : "%s\n")).
                                 + (valueCheck(x.newValue()) + "\n")).
-                                formatted(x.key(), x.newValue())
+                                formatted(x.key())
                 );
                 case DELETED -> result.append(
                         (FIRST_LINE_PART + "removed\n").formatted(x.key())
@@ -34,7 +34,7 @@ public class PlainFormatter {
                                 + (valueCheck(x.oldValue()) + " to ")
                                 + (valueCheck(x.newValue()) + "\n")
                         ).
-                        formatted(x.key(), x.oldValue(), x.newValue())
+                        formatted(x.key())
                 );
                 default -> {
                 }
@@ -59,13 +59,13 @@ public class PlainFormatter {
 
     public static String valueCheck(Object obj) {
         if (isString(obj)) {
-            System.out.println(obj + " is string, its class is " + (obj != null ? obj.getClass() : "null"));
-            return "'%s'";
+//            System.out.println(obj + " is string, its class is " + (obj != null ? obj.getClass() : "null"));
+            return "'%s'".formatted(obj);
         } else if (isComplex(obj)) {
-            System.out.println(obj + " is complex value, its class is " + (obj != null ? obj.getClass() : "null"));
+//            System.out.println(obj + " is complex value, its class is " + (obj != null ? obj.getClass() : "null"));
             return "[complex value]";
         }
-        System.out.println(obj + " is primitive or wrapper, its class is " + (obj != null ? obj.getClass() : "null"));
-        return "%s";
+//        System.out.println(obj + " is primitive or wrapper, its class is " + (obj != null ? obj.getClass() : "null"));
+        return "%s".formatted(obj);
     }
 }
