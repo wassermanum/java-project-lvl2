e0a8dc51cd29088edb7fbbe95460df22a390a108
Dmitri Chuprin <wassermanum@gmail.com>
2022-08-18 03:57:22 +0600

    reformat code, add asciinema

--- README.md
+++ README.md
@@ -1,6 +1,10 @@
 ### Hexlet tests and linter status:
 [![Actions Status](https://github.com/wassermanum/java-project-lvl2/workflows/hexlet-check/badge.svg)](https://github.com/wassermanum/java-project-lvl2/actions)
 
+[![Java CI](https://github.com/wassermanum/java-project-lvl2/actions/workflows/gradle-build.yml/badge.svg)](https://github.com/wassermanum/java-project-lvl2/actions/workflows/gradle-build.yml)
+
 [![Maintainability](https://api.codeclimate.com/v1/badges/4444f431828d99a51ba6/maintainability)](https://codeclimate.com/github/wassermanum/java-project-lvl2/maintainability)
 
 [![Test Coverage](https://api.codeclimate.com/v1/badges/4444f431828d99a51ba6/test_coverage)](https://codeclimate.com/github/wassermanum/java-project-lvl2/test_coverage)
+
+[![asciicast](https://asciinema.org/a/515570.svg)](https://asciinema.org/a/515570)
--- src/main/java/hexlet/code/formatters/PlainFormatter.java
+++ src/main/java/hexlet/code/formatters/PlainFormatter.java
@@ -20,7 +20,6 @@ public class PlainFormatter {
             switch (x.status()) {
                 case ADDED -> result.append(
                         (FIRST_LINE_PART + "added with value: "
-//                                + (isString(x.newValue()) ? "'%s'\n" : "%s\n")).
                                 + (valueCheck(x.newValue()) + "\n")).
                                 formatted(x.key())
                 );
@@ -29,8 +28,6 @@ public class PlainFormatter {
                 );
                 case CHANGED -> result.append(
                         (FIRST_LINE_PART + "updated. From "
-//                                + (isString(x.oldValue()) ? "'%s' to " : "%s to ")
-//                                + (isString(x.newValue()) ? "'%s'\n" : "%s\n")
                                 + (valueCheck(x.oldValue()) + " to ")
                                 + (valueCheck(x.newValue()) + "\n")
                         ).
@@ -59,13 +56,10 @@ public class PlainFormatter {
 
     public static String valueCheck(Object obj) {
         if (isString(obj)) {
-//            System.out.println(obj + " is string, its class is " + (obj != null ? obj.getClass() : "null"));
             return "'%s'".formatted(obj);
         } else if (isComplex(obj)) {
-//            System.out.println(obj + " is complex value, its class is " + (obj != null ? obj.getClass() : "null"));
             return "[complex value]";
         }
-//        System.out.println(obj + " is primitive or wrapper, its class is " + (obj != null ? obj.getClass() : "null"));
         return "%s".formatted(obj);
     }
 }
