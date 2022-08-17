1621d3ff1f4e5bfe08fffc10760385b27ebec518
Dmitri Chuprin <wassermanum@gmail.com>
2022-08-18 00:29:05 +0600

    debug

--- src/main/java/hexlet/code/formatters/PlainFormatter.java
+++ src/main/java/hexlet/code/formatters/PlainFormatter.java
@@ -59,10 +59,13 @@ public class PlainFormatter {
 
     public static String valueCheck(Object obj) {
         if (isString(obj)) {
+            System.out.println(obj + " is string, its class is " + obj.getClass());
             return "'%s'";
         } else if (isComplex(obj)) {
+            System.out.println(obj + " is complex value, its class is " + obj.getClass());
             return "[complex value]";
         }
+        System.out.println(obj + " is primitive or wrapper, its class is " + obj.getClass());
         return "%s";
     }
 }
